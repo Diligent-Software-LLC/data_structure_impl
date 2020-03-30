b9e3e3d9bc0dc9f55919d6e09bdf59734760333d
Bradley J. Tannor <bradleytannor@gmail.com>
2020-03-30 13:13:32 -0700

    Build Bug
    Interface never installed.

--- lib/data_structure_impl/version.rb
+++ lib/data_structure_impl/version.rb
@@ -1,11 +1,11 @@
 # Copyright (C) 2020 Diligent Software LLC. All rights reserved. Released
 # under the GNU General Public License, Version 3. Refer LICENSE.txt.
 
-require 'data_structure_int'
+#require 'data_structure_int'
 
 # DataStructure.
 # A DataStructure type implementation. Implements the DataStructure type
 # interface.
-class DataStructure < DataStructureInt
+class DataStructure #< DataStructureInt
   VERSION = '0.1.0'.freeze()
 end
